9e9da929c3bc9ee823c4a0fb1de5cd986a655e68
CYann <wcy623209668@vip.qq.com>
2017-06-07 21:43:22 +0800

    将Helloworld的demo进行更改，使用Express 搭建了关于Graphql的服务器端网页，测试demo完成的十分成功

--- test/server.js
+++ test/server.js
@@ -1,17 +1,26 @@
-var { graphql , buildSchema } = require ('graphql');
+var express = require ('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require ('graphql');
 
+//建立一个Schema ， 使用Graphql Schema 语言
 var schema = buildSchema(`
     type Query {
         hello: String
     }
 `);
-
+//root对每一个API断点都提供了一个resolver功能
 var root = {
     hello : ( ) =>{
         return 'Hello World' ;
     },
 };
 
-graphql(schema , '{hello}' , root).then((response)=>{
-    console.log(response);
-})
+var app = express();
+app.use('/graphql' , graphqlHTTP({
+    schema:schema ,
+    rootValue:root,
+    graphiql:true,
+}));
+
+app.listen(4000);
+console.log('Running a Graphql API server at localhost:4000/graphql');
