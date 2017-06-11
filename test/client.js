7ebf1031bad0ed992cfa7f40802318a913866443
CYann <wcy623209668@vip.qq.com>
2017-06-11 20:16:04 +0800

    [添加] 增加 mutation用法的例子 和增加数据例子

--- test/client.js
+++ test/client.js
@@ -12,7 +12,7 @@ xhr.onload = function(){
 xhr.send(JSON.stringify({query:"{hello}"}));
 */
 
-//测试应对复杂数据客户端的使用和编写
+/*//测试应对复杂数据客户端的使用和编写
 
 var dice = 3;
 var sides = 6;
@@ -30,4 +30,33 @@ var query = `query RollDice($dice: Int!, $sides: Int) {
 xhr.send(JSON.stringify({
   query: query,
   variables: { dice: dice, sides: sides },
+}));
+*/
+
+/**
+ * 更改和输入类型测试client.js
+ */
+var author = 'andy';
+var content = 'hope is a good thing';
+var xhr = new XMLHttpRequest();
+xhr.responseType = 'json';
+xhr.open("POST", "/graphql");
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Accept", "application/json");
+xhr.onload = function () {
+  console.log('data returned:', xhr.response);
+}
+var query = `mutation CreateMessage($input: MessageInput) {
+  createMessage(input: $input) {
+    id
+  }
+}`;
+xhr.send(JSON.stringify({
+  query: query,
+  variables: {
+    input: {
+      author: author,
+      content: content,
+    }
+  }
 }));
--- test/server.js
+++ test/server.js
@@ -66,7 +66,7 @@ console.log('Running a GraphQL API server at localhost:4000/graphql');
  * 基础类型测试server.js
  */
 
-var express = require('express');
+/*var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
@@ -99,4 +99,79 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+console.log('Running a GraphQL API server at localhost:4000/graphql');*/
+
+/**
+ * 更改和输入类型测试server.js
+ */
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+//建立一个Schema ， 使用Graphql Schema 语言
+var schema = buildSchema(`
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Query {
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
+`);
+
+// 将消息赋值在这个对象上
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+var fakeDatabase = {};
+//root对每一个API断点都提供了一个resolver功能
+var root = {
+  getMessage: function ({id}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: function ({input}) {
+    //创建一个随机的id赋给我们创建Schema
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: function ({id, input}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    //替换掉所有的旧的数据
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+}
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => {
+  console.log('Running a GraphQL API server at localhost:4000/graphql');
+});
